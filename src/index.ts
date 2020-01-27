06ecbe9bb53afa7bbded2eb769142b9f5437b7fe
Marcin Wydrzyski <wydrzynskimarcin@gmail.com>
2020-01-27 21:26:38 +0100

    better error handling

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,34 @@ const visitor = new Visitor()
 
 export interface IFormatingOptions {
     indent?: string,
+    newLineSeparator?: string
     maxStatementLength?: number
 }
 
 const defaultOptions: Required<IFormatingOptions> = {
     indent: '\t',
+    newLineSeparator: '\n',
     maxStatementLength: 80
 }
 
-export function formatNginxConfig(
+export function nginxFormat(
     configString: string,
-    options: IFormatingOptions
+    options: IFormatingOptions = {}
 ): string {
 
     const mergedOptions = { ...defaultOptions, ...options }
 
     const lexingResult = Lexer.tokenize(configString)
+    if(lexingResult.errors.length) {
+        throw new Error('Syntax Error: ' + JSON.stringify(lexingResult.errors))
+    }
     Parser.input = lexingResult.tokens
+    
     const ctx = (Parser as any).Block()
+        if(Parser.errors.length) {
+            throw new Error('Parsing Error:' + JSON.stringify(Parser.errors))
+        }
     const ast = visitor.visit(ctx)
-
 
     return new FormatedEmitter(mergedOptions).generateOutput(ast, lexingResult.groups.Comment)
 
